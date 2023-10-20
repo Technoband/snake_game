00cc8d972989c8a6eb3c8a00778a5ae4ee918243
Yashraj Zope <105965863+Technoband@users.noreply.github.com>
2023-10-20 11:24:54 +0530

    adding snake game
    I have used Pycharm latest version to for this code, so kindly use Pycharm for this project with its new version environment  so that you will not face any problem while coding

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("Yellow")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-200, 200)
+        random_y = random.randint(-200, 200)
+        self.goto(random_x, random_y)
